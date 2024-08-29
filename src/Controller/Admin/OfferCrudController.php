5d9af622d69a19a2f4bdeb830126170aa7f3ab3b
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-08-29 16:09:33 +0200

    admin update

---
+++ src/Controller/Admin/OfferCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Offer;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class OfferCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Offer::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title', 'Titre'),
+            BooleanField::new('visible'),
+            DateTimeField::new('postDate', 'Date de publication'),
+            TextEditorField::new('description', 'Description'),
+            AssociationField::new('location', 'Ville')->autocomplete(),
+        ];
+    }
+
+}
